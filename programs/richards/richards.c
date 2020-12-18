81e08cad30a143b1c65193d96e7847df79b91f5d
Stefane Fermigier <sf@fermigier.com>
2020-12-18 14:37:34 +0100

    Format.

--- programs/richards/richards.c
+++ programs/richards/richards.c
@@ -1,100 +1,98 @@
-
 /*  C version of the systems programming language benchmark
-**  Author:  M. J. Jordan  Cambridge Computer Laboratory.
-**
-**  Modified by:  M. Richards, Nov 1996
-**    to be ANSI C and runnable on 64 bit machines + other minor changes
-**  Modified by:  M. Richards, 20 Oct 1998
-**    made minor corrections to improve ANSI compliance (suggested
-**    by David Levine)
-**
-**  Compile with, say
-**
-**  gcc -o bench bench.c
-**
-**  or
-**
-**  gcc -o bench100 -Dbench100 bench.c  (for a version that obeys
-**                                       the main loop 100x more often)
-*/
+ **  Author:  M. J. Jordan  Cambridge Computer Laboratory.
+ **
+ **  Modified by:  M. Richards, Nov 1996
+ **    to be ANSI C and runnable on 64 bit machines + other minor changes
+ **  Modified by:  M. Richards, 20 Oct 1998
+ **    made minor corrections to improve ANSI compliance (suggested
+ **    by David Levine)
+ **
+ **  Compile with, say
+ **
+ **  gcc -o bench bench.c
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/* #include "harness.h" */
 
-#ifdef bench100
-#define                Count           10000*100
-#define                Qpktcountval    2326410
-#define                Holdcountval     930563
-#else
-#define                Count           10000
-#define                Qpktcountval    23246
-#define                Holdcountval     9297
-#endif
+#define Count 10000
+#define Qpktcountval 23246
+#define Holdcountval 9297
 
-#define                TRUE            1
-#define                FALSE           0
-#define                MAXINT          32767
+#define TRUE 1
+#define FALSE 0
+#define MAXINT 32767
 
-#define                BUFSIZE         3
-#define                I_IDLE          1
-#define                I_WORK          2
-#define                I_HANDLERA      3
-#define                I_HANDLERB      4
-#define                I_DEVA          5
-#define                I_DEVB          6
-#define                PKTBIT          1
-#define                WAITBIT         2
-#define                HOLDBIT         4
-#define                NOTPKTBIT       !1
-#define                NOTWAITBIT      !2
-#define                NOTHOLDBIT      0XFFFB
+#define BUFSIZE 3
+#define I_IDLE 1
+#define I_WORK 2
+#define I_HANDLERA 3
+#define I_HANDLERB 4
+#define I_DEVA 5
+#define I_DEVB 6
+#define PKTBIT 1
+#define WAITBIT 2
+#define HOLDBIT 4
+#define NOTPKTBIT !1
+#define NOTWAITBIT !2
+#define NOTHOLDBIT 0XFFFB
 
-#define                S_RUN           0
-#define                S_RUNPKT        1
-#define                S_WAIT          2
-#define                S_WAITPKT       3
-#define                S_HOLD          4
-#define                S_HOLDPKT       5
-#define                S_HOLDWAIT      6
-#define                S_HOLDWAITPKT   7
+#define S_RUN 0
+#define S_RUNPKT 1
+#define S_WAIT 2
+#define S_WAITPKT 3
+#define S_HOLD 4
+#define S_HOLDPKT 5
+#define S_HOLDWAIT 6
+#define S_HOLDWAITPKT 7
 
-#define                K_DEV           1000
-#define                K_WORK          1001
+#define K_DEV 1000
+#define K_WORK 1001
 
-struct packet
-{
-    struct packet  *p_link;
-    int             p_id;
-    int             p_kind;
-    int             p_a1;
-    char            p_a2[BUFSIZE+1];
+struct packet {
+    struct packet *p_link;
+    int p_id;
+    int p_kind;
+    int p_a1;
+    char p_a2[BUFSIZE + 1];
 };
 
-struct task
-{
-    struct task    *t_link;
-    int             t_id;
-    int             t_pri;
-    struct packet  *t_wkq;
-    int             t_state;
-    struct task    *(*t_fn)(struct packet *);
-    long            t_v1;
-    long            t_v2;
+struct task {
+    struct task *t_link;
+    int t_id;
+    int t_pri;
+    struct packet *t_wkq;
+    int t_state;
+    struct task *(*t_fn) (struct packet *);
+    long t_v1;
+    long t_v2;
 };
 
-char  alphabet[28] = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-struct task *tasktab[11]  =  {(struct task *)10,0,0,0,0,0,0,0,0,0,0};
-struct task *tasklist    =  0;
+char alphabet[28] = "0ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+struct task *tasktab[11] = {
+    (struct task *) 10,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+};
+
+struct task *tasklist = 0;
 struct task *tcb;
-long    taskid;
-long    v1;
-long    v2;
-int     qpktcount    =  0;
-int     holdcount    =  0;
-int     tracing      =  0;
-int     layout       =  0;
+long taskid;
+long v1;
+long v2;
+int qpktcount = 0;
+int holdcount = 0;
+int tracing = 0;
+int layout = 0;
 
 void append(struct packet *pkt, struct packet *ptr);
 
@@ -102,30 +100,30 @@ void createtask(int id,
                 int pri,
                 struct packet *wkq,
                 int state,
-                struct task *(*fn)(struct packet *),
+                struct task *(*fn) (struct packet *),
                 long v1,
                 long v2)
 {
-    struct task *t = (struct task *)malloc(sizeof(struct task));
+    struct task *t = (struct task *) malloc(sizeof(struct task));
 
     tasktab[id] = t;
-    t->t_link   = tasklist;
-    t->t_id     = id;
-    t->t_pri    = pri;
-    t->t_wkq    = wkq;
-    t->t_state  = state;
-    t->t_fn     = fn;
-    t->t_v1     = v1;
-    t->t_v2     = v2;
-    tasklist    = t;
+    t->t_link = tasklist;
+    t->t_id = id;
+    t->t_pri = pri;
+    t->t_wkq = wkq;
+    t->t_state = state;
+    t->t_fn = fn;
+    t->t_v1 = v1;
+    t->t_v2 = v2;
+    tasklist = t;
 }
 
 struct packet *pkt(struct packet *link, int id, int kind)
 {
     int i;
-    struct packet *p = (struct packet *)malloc(sizeof(struct packet));
+    struct packet *p = (struct packet *) malloc(sizeof(struct packet));
 
-    for (i=0; i<=BUFSIZE; i++)
+    for (i = 0; i <= BUFSIZE; i++)
         p->p_a2[i] = 0;
 
     p->p_link = link;
@@ -138,8 +136,7 @@ struct packet *pkt(struct packet *link, int id, int kind)
 
 void trace(char a)
 {
-   if ( --layout <= 0 )
-   {
+    if (--layout <= 0) {
         printf("\n");
         layout = 50;
     }
@@ -149,44 +146,42 @@ void trace(char a)
 
 void schedule()
 {
-    while ( tcb != 0 )
-    {
+    while (tcb != 0) {
         struct packet *pkt;
         struct task *newtcb;
 
-        pkt=0;
+        pkt = 0;
 
-        switch ( tcb->t_state )
-        {
-            case S_WAITPKT:
-                pkt = tcb->t_wkq;
-                tcb->t_wkq = pkt->p_link;
-                tcb->t_state = tcb->t_wkq == 0 ? S_RUN : S_RUNPKT;
+        switch (tcb->t_state) {
+        case S_WAITPKT:
+            pkt = tcb->t_wkq;
+            tcb->t_wkq = pkt->p_link;
+            tcb->t_state = tcb->t_wkq == 0 ? S_RUN : S_RUNPKT;
 
-            case S_RUN:
-            case S_RUNPKT:
-                taskid = tcb->t_id;
-                v1 = tcb->t_v1;
-                v2 = tcb->t_v2;
-                if (tracing) {
-		  trace(taskid+'0');
-		}
-                newtcb = (*(tcb->t_fn))(pkt);
-                tcb->t_v1 = v1;
-                tcb->t_v2 = v2;
-                tcb = newtcb;
-                break;
+        case S_RUN:
+        case S_RUNPKT:
+            taskid = tcb->t_id;
+            v1 = tcb->t_v1;
+            v2 = tcb->t_v2;
+            if (tracing) {
+                trace(taskid + '0');
+            }
+            newtcb = (*(tcb->t_fn)) (pkt);
+            tcb->t_v1 = v1;
+            tcb->t_v2 = v2;
+            tcb = newtcb;
+            break;
 
-            case S_WAIT:
-            case S_HOLD:
-            case S_HOLDPKT:
-            case S_HOLDWAIT:
-            case S_HOLDWAITPKT:
-                tcb = tcb->t_link;
-                break;
+        case S_WAIT:
+        case S_HOLD:
+        case S_HOLDPKT:
+        case S_HOLDWAIT:
+        case S_HOLDWAITPKT:
+            tcb = tcb->t_link;
+            break;
 
-            default:
-                return;
+        default:
+            return;
         }
     }
 }
@@ -201,17 +196,18 @@ struct task *holdself(void)
 {
     ++holdcount;
     tcb->t_state |= HOLDBIT;
-    return (tcb->t_link) ;
+    return (tcb->t_link);
 }
 
 struct task *findtcb(int id)
 {
     struct task *t = 0;
 
-    if (1<=id && id<=(long)tasktab[0])
-    t = tasktab[id];
-    if (t==0) printf("\nBad task id %d\n", id);
-    return(t);
+    if (1 <= id && id <= (long) tasktab[0])
+        t = tasktab[id];
+    if (t == 0)
+        printf("\nBad task id %d\n", id);
+    return (t);
 }
 
 struct task *release(int id)
@@ -219,36 +215,36 @@ struct task *release(int id)
     struct task *t;
 
     t = findtcb(id);
-    if ( t==0 ) return (0);
+    if (t == 0)
+        return (0);
 
     t->t_state &= NOTHOLDBIT;
-    if ( t->t_pri > tcb->t_pri ) return (t);
+    if (t->t_pri > tcb->t_pri)
+        return (t);
 
-    return (tcb) ;
+    return (tcb);
 }
-
 
 struct task *qpkt(struct packet *pkt)
 {
     struct task *t;
 
     t = findtcb(pkt->p_id);
-    if (t==0) return (t);
+    if (t == 0)
+        return (t);
 
     qpktcount++;
 
     pkt->p_link = 0;
     pkt->p_id = taskid;
 
-   if (t->t_wkq==0)
-    {
+    if (t->t_wkq == 0) {
         t->t_wkq = pkt;
         t->t_state |= PKTBIT;
-        if (t->t_pri > tcb->t_pri) return (t);
-    }
-    else
-    {
-        append(pkt, (struct packet *)&(t->t_wkq));
+        if (t->t_pri > tcb->t_pri)
+            return (t);
+    } else {
+        append(pkt, (struct packet *) &(t->t_wkq));
     }
 
     return (tcb);
@@ -256,84 +252,81 @@ struct task *qpkt(struct packet *pkt)
 
 struct task *idlefn(struct packet *pkt)
 {
-    if ( --v2==0 ) return ( holdself() );
+    if (--v2 == 0)
+        return (holdself());
 
-    if ( (v1&1) == 0 )
-    {
-        v1 = ( v1>>1) & MAXINT;
-        return ( release(I_DEVA) );
-    }
-    else
-    {
-        v1 = ( (v1>>1) & MAXINT) ^ 0XD008;
-        return ( release(I_DEVB) );
+    if ((v1 & 1) == 0) {
+        v1 = (v1 >> 1) & MAXINT;
+        return (release(I_DEVA));
+    } else {
+        v1 = ((v1 >> 1) & MAXINT) ^ 0XD008;
+        return (release(I_DEVB));
     }
 }
 
 struct task *workfn(struct packet *pkt)
 {
-    if ( pkt==0 ) return ( wait_task() );
-    else
-    {
+    if (pkt == 0)
+        return (wait_task());
+    else {
         int i;
 
         v1 = I_HANDLERA + I_HANDLERB - v1;
         pkt->p_id = v1;
 
         pkt->p_a1 = 0;
-        for (i=0; i<=BUFSIZE; i++)
-        {
+        for (i = 0; i <= BUFSIZE; i++) {
             v2++;
-            if ( v2 > 26 ) v2 = 1;
+            if (v2 > 26)
+                v2 = 1;
             (pkt->p_a2)[i] = alphabet[v2];
         }
-        return ( qpkt(pkt) );
+        return (qpkt(pkt));
     }
 }
 
 struct task *handlerfn(struct packet *pkt)
 {
-  if ( pkt!=0) {
-    append(pkt, (struct packet *)(pkt->p_kind==K_WORK ? &v1 : &v2));
-  }
-
-  if ( v1!=0 ) {
-    int count;
-    struct packet *workpkt = (struct packet *)v1;
-    count = workpkt->p_a1;
-
-    if ( count > BUFSIZE ) {
-      v1 = (long)(((struct packet *)v1)->p_link);
-      return ( qpkt(workpkt) );
+    if (pkt != 0) {
+        append(pkt, (struct packet *) (pkt->p_kind == K_WORK ? &v1 : &v2));
     }
 
-    if ( v2!=0 ) {
-      struct packet *devpkt;
+    if (v1 != 0) {
+        int count;
+        struct packet *workpkt = (struct packet *) v1;
+        count = workpkt->p_a1;
 
-      devpkt = (struct packet *)v2;
-      v2 = (long)(((struct packet *)v2)->p_link);
-      devpkt->p_a1 = workpkt->p_a2[count];
-      workpkt->p_a1 = count+1;
-      return( qpkt(devpkt) );
+        if (count > BUFSIZE) {
+            v1 = (long) (((struct packet *) v1)->p_link);
+            return (qpkt(workpkt));
+        }
+
+        if (v2 != 0) {
+            struct packet *devpkt;
+
+            devpkt = (struct packet *) v2;
+            v2 = (long) (((struct packet *) v2)->p_link);
+            devpkt->p_a1 = workpkt->p_a2[count];
+            workpkt->p_a1 = count + 1;
+            return (qpkt(devpkt));
+        }
     }
-  }
-  return wait_task();
+    return wait_task();
 }
 
 struct task *devfn(struct packet *pkt)
 {
-    if ( pkt==0 )
-    {
-        if ( v1==0 ) return ( wait_task() );
-        pkt = (struct packet *)v1;
+    if (pkt == 0) {
+        if (v1 == 0)
+            return (wait_task());
+        pkt = (struct packet *) v1;
         v1 = 0;
-        return ( qpkt(pkt) );
-    }
-    else
-    {
-        v1 = (long)pkt;
-        if (tracing) trace(pkt->p_a1);
-        return ( holdself() );
+        return (qpkt(pkt));
+    } else {
+        v1 = (long) pkt;
+        if (tracing)
+            trace(pkt->p_a1);
+        return (holdself());
     }
 }
 
@@ -341,15 +334,15 @@ void append(struct packet *pkt, struct packet *ptr)
 {
     pkt->p_link = 0;
 
-    while ( ptr->p_link ) ptr = ptr->p_link;
+    while (ptr->p_link)
+        ptr = ptr->p_link;
 
     ptr->p_link = pkt;
 }
 
-int bench() {
+int bench()
+{
     struct packet *wkq = 0;
-
-    // printf("Bench mark starting\n");
 
     createtask(I_IDLE, 0, wkq, S_RUN, idlefn, 1, Count);
 
@@ -378,29 +371,21 @@ int bench() {
 
     qpktcount = holdcount = 0;
 
-    // printf("Starting\n");
-
     tracing = FALSE;
     layout = 0;
 
     schedule();
-
-    // printf("\nfinished\n");
-
-
-
 
     if (!(qpktcount == Qpktcountval && holdcount == Holdcountval)) {
         printf("qpkt count = %d  holdcount = %d\n", qpktcount, holdcount);
         printf("These results are incorrect");
         exit(1);
     }
-    // printf("\nend of run\n");
     return qpktcount;
 }
 
-
-int inner_loop(int inner) {
+int inner_loop(int inner)
+{
     int r = 0;
     while (inner > 0) {
         r += bench();
@@ -409,10 +394,10 @@ int inner_loop(int inner) {
     return r;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     int iterations = 1;
-    int warmup     = 0;
+    int warmup = 0;
     int inner_iterations = 50;
 
     int result = 0;
