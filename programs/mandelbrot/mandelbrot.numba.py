180cf42ba2f7fdbc4949c46719ad10401a66c6e8
Stefane Fermigier <sf@fermigier.com>
2020-12-19 15:23:58 +0100

    Format

--- programs/mandelbrot/mandelbrot.numba.py
+++ programs/mandelbrot/mandelbrot.numba.py
@@ -16,7 +16,7 @@ HALF = False
 
 # We knowingly produce overflows, therefore
 # ignore warning
-np.seterr(all='ignore')
+np.seterr(all="ignore")
 
 size = int(sys.argv[1])
 if HALF:
@@ -30,16 +30,16 @@ remin = -1.5
 remax = 0.5
 # if we break the symmetry here, we have to fix the "calculate half" aspect
 # of the code!
-imin = -1.
+imin = -1.0
 imax = -imin
 
 # there are apparently a few differences in handling strings and byte arrays...
-V3 = sys.version_info >= (3, )
+V3 = sys.version_info >= (3,)
 
 # maximum number of points calculated in parallel to avoid
 # excessive memory use
 # this seems to be the optimum on my machine, fairly low value imho
-maxchunk = 1<<14
+maxchunk = 1 << 14
 
 # fixed to match reference output
 # iterations = 49
@@ -48,36 +48,36 @@ maxchunk = 1<<14
 # optimized for speed, ignoring overflows
 # this is apparently a good tradeoff between the cost of making a new array
 # and the cost of calculating more points
-iteration_steps = [9] + [10]*4
+iteration_steps = [9] + [10] * 4
 
 # distance between points in real direction
-restep = (remax - remin)/size
+restep = (remax - remin) / size
 # distance between points in imaginary direction
-imstep = (imax - imin)/size
+imstep = (imax - imin) / size
 
 # number of bits
 # remainder of experiments with long long int for bit shifting,
 # but no advantage, therefore byte boundary of lines not implemented for
 # other types than unsigned byte
-(bits, calctype) = (8, 'u1')
+(bits, calctype) = (8, "u1")
 bytesize = 8
 
 # precalculate real part of one line  and reuse later
 reline = np.linspace(remin, remax - restep, size)
 # precalculate imaginary parts and reuse later
 if HALF:
-    imline = 1j*np.linspace(imin, 0 - imstep*(size%2)*0.5, calcsize)
+    imline = 1j * np.linspace(imin, 0 - imstep * (size % 2) * 0.5, calcsize)
 else:
-    imline = 1j*np.linspace(imin, imax - imstep, calcsize)
+    imline = 1j * np.linspace(imin, imax - imstep, calcsize)
 
 # padding to start new line at byte boundary following pbm format
 # size in [1,8] -> 1 byte per line ; size in [9,16]-> 2 bytes per line etc.
-linesize = (size - 1)//bytesize + 1
+linesize = (size - 1) // bytesize + 1
 
 # PBM header, important to calculate buffer size
 header = "P4\n{0} {0}\n".format(size)
 if V3:
-    header = bytes(header, 'ascii')
+    header = bytes(header, "ascii")
 header_offset = len(header)
 
 bufferlen = len(header) + linesize * size
@@ -91,7 +91,7 @@ bufferlen = len(header) + linesize * size
 # if we wanted bigger than about 50000x50000, or minimize memory usage, it could
 # however be a good idea to implement the mmap version.
 sharedbitmap = multiprocessing.Array(ctypes.c_char, bufferlen, lock=False)
-sharedbitmap[0:len(header)] = header
+sharedbitmap[0 : len(header)] = header
 
 # not more to avoid task switching overhead and surplus memory usage
 # one process already puts one core to 100%, no waiting, locking etc. :-)
@@ -104,16 +104,16 @@ maxlines = maxchunk // size
 # make sure we calculate at least one line per package
 
 # we only calculate the upper half of the set and exploit it's symmetry!
-lines_per_chunk = max(1, min(calcsize//workers,  maxlines))
+lines_per_chunk = max(1, min(calcsize // workers, maxlines))
 
 # number of tasks, only upper half of the set!
 packages = max(1, calcsize // lines_per_chunk)
 # hehe, we could have many processors...
 if workers <= calcsize:
-# make sure it's dividable by number of processors to have all working
-# There's a small imbalance at the very end of the program run because
-# different chunks have different calculation time
-    packages += packages%workers
+    # make sure it's dividable by number of processors to have all working
+    # There's a small imbalance at the very end of the program run because
+    # different chunks have different calculation time
+    packages += packages % workers
 else:
     # one line per package
     packages = calcsize
@@ -123,13 +123,13 @@ lines_per_chunk = max(1, calcsize // packages)
 
 tasks = []
 for i in range(packages):
-    tasks.append((i*lines_per_chunk, lines_per_chunk))
+    tasks.append((i * lines_per_chunk, lines_per_chunk))
 
 # see what lines are not covered yet, distribute them among the tasks
 (last_offset, last_lines) = tasks[-1]
-missing_lines = calcsize - (last_offset+last_lines)
+missing_lines = calcsize - (last_offset + last_lines)
 for i in range(missing_lines):
-    index = -(missing_lines-i)
+    index = -(missing_lines - i)
     (offset, lines) = tasks[index]
     tasks[index] = (offset + i, lines + 1)
 
@@ -167,40 +167,48 @@ def mandeliterations(z):
     # keep bitmap business in pbmline()
     z[mandelbrot] = 0
 
+
 # generate/allocate block of complex numbers for subsequent iteration
 @autojit
 def mandelblock(line_offset, lines):
     # maybe numpy.mgrid or another method would be faster, but not tried yet...
-    (re, im) = np.meshgrid(reline, imline[line_offset:line_offset+lines])
+    (re, im) = np.meshgrid(reline, imline[line_offset : line_offset + lines])
     # reuse memory
     im += re
     return im
+
 
 # convert data array into "compressed" bitmap to be written to binary pbm file
 @autojit
 def pbmline(points, lines):
     # each point is in [0, 1] now, 8 bit unsigned int
-    bitmap = np.zeros((lines, linesize*bits), dtype=calctype)
+    bitmap = np.zeros((lines, linesize * bits), dtype=calctype)
     # respect  the "padding" bits at the end of
     # the line to get byte boundaries
-    bitmap[:,:size] = points==0
+    bitmap[:, :size] = points == 0
     # make blocks with 8 bits
-    bitmap = bitmap.reshape((linesize*lines, bits))
+    bitmap = bitmap.reshape((linesize * lines, bits))
     # shift bits, accumulate in highest bit
-    for bit in range(0,bits-1):
+    for bit in range(0, bits - 1):
         # fix bit order here
-        bitmap[:,bit] <<= (bits-bit-1)
-        bitmap[:,bits-1] += bitmap[:,bit]
+        bitmap[:, bit] <<= bits - bit - 1
+        bitmap[:, bits - 1] += bitmap[:, bit]
     # return accumulator
-    result = bitmap[:,bits-1]
+    result = bitmap[:, bits - 1]
     return result
 
+
 if V3:
+
     def tobytes(a):
         return bytes(iter(a))
+
+
 else:
+
     def tobytes(a):
         return a.tostring()
+
 
 if HALF:
     # move bitmap fragments to output
@@ -214,11 +222,11 @@ if HALF:
         startbyte = header_offset + line_offset * linesize
         # program works with 8/8 now, less headache
         # but keep this for explicity
-        copybytes = len_bitmap*bits//bytesize
+        copybytes = len_bitmap * bits // bytesize
         # didn't get ctypes.memcopy to work,
         # this does it although it may be a bit slower
         # bitmap HAS to be flat
-        sharedbitmap[startbyte:startbyte+len_bitmap] = tobytes(bitmap)
+        sharedbitmap[startbyte : startbyte + len_bitmap] = tobytes(bitmap)
         # now reshape to lines x lines in order to reverse and exploit symmetry
         bitmap = bitmap.reshape((lines, linesize))
         # reverse the lines
@@ -228,14 +236,16 @@ if HALF:
         # the calculated area is not symmetric, we have overlap
         # therefore clip overhang
         if startbyte < (bufferlen - header_offset) // 2:
-            bitmap = bitmap[1:,:]
+            bitmap = bitmap[1:, :]
             startbyte += linesize
         if stopbyte > bufferlen:
-            bitmap = bitmap[:-1,:]
+            bitmap = bitmap[:-1, :]
             stopbyte -= linesize
         # flat again for output
         bitmap = bitmap.reshape(-1)
-        sharedbitmap[startbyte:startbyte+len_bitmap] = tobytes(bitmap)
+        sharedbitmap[startbyte : startbyte + len_bitmap] = tobytes(bitmap)
+
+
 else:
     # move bitmap fragments to output
     # eventually modifies bitmap in the process!
@@ -248,11 +258,12 @@ else:
         startbyte = header_offset + line_offset * linesize
         # program works with 8/8 now, less headache
         # but keep this for explicity
-        copybytes = len_bitmap*bits//bytesize
+        copybytes = len_bitmap * bits // bytesize
         # didn't get ctypes.memcopy to work,
         # this does it although it may be a bit slower
         # bitmap HAS to be flat
-        sharedbitmap[startbyte:startbyte+len_bitmap] = tobytes(bitmap)
+        sharedbitmap[startbyte : startbyte + len_bitmap] = tobytes(bitmap)
+
 
 # function to be called with element of the task array,
 # suitable for mapping (no output, only modifies shared buffer)
@@ -263,14 +274,14 @@ def work(tup):
     bitmap = pbmline(block, lines)
     copybits(line_offset, bitmap, lines)
 
+
 pool = multiprocessing.Pool(workers)
 # for debugging: just use map(...) instead of pool.map(...)
 # to get the exceptions + trace
 # global variables etc. are shared between processes! :-)
 pool.map(work, tasks)
-#for tup in tasks:
-    #work(tup)
-
+# for tup in tasks:
+# work(tup)
 
 
 # dump it!
--- programs/mandelbrot/mandelbrot.py
+++ programs/mandelbrot/mandelbrot.py
@@ -13,37 +13,39 @@ V = 0.0
 K = 1.5
 k = 1.0
 
+
 def mandelbrot():
-	global h, Z,z,T,t,C,c, U,V
-	y = 0
-	while y < 150:
-		y += 1
-		x = 0
-		while x < 150:
-			x += 1
-			Z, z, T, t = 0.0, 0.0, 0.0, 0.0
-			U = x*2
-			U /= h
-			V = y*2
-			V /= h
-			C = U - K
-			c = V - k
+    global h, Z, z, T, t, C, c, U, V
+    y = 0
+    while y < 150:
+        y += 1
+        x = 0
+        while x < 150:
+            x += 1
+            Z, z, T, t = 0.0, 0.0, 0.0, 0.0
+            U = x * 2
+            U /= h
+            V = y * 2
+            V /= h
+            C = U - K
+            c = V - k
 
-			i = 0
-			while i < 50:
-				i += 1
-				if T+t <= 4:
-					z = Z*z
-					z *= 2
-					z += c
-					Z = T - t
-					Z += C
-					T = Z * Z
-					t = z * z
+            i = 0
+            while i < 50:
+                i += 1
+                if T + t <= 4:
+                    z = Z * z
+                    z *= 2
+                    z += c
+                    Z = T - t
+                    Z += C
+                    T = Z * Z
+                    t = z * z
 
-			if T+t <= 4:
-				print('*', end='')
-			else:
-				print('·', end='')
+            if T + t <= 4:
+                print("*", end="")
+            else:
+                print("·", end="")
+
 
 mandelbrot()
