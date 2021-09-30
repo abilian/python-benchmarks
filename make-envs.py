e81ff156a34883323214eea9824a4b23a2405911
Stefane Fermigier <sf@abilian.com>
2021-09-30 12:23:11 +0200

    feat: faster envs creation.

--- make-envs.py
+++ make-envs.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 from dataclasses import dataclass
+from pathlib import Path
 
 import fire
+import rich
 from plumbum import local
 import config
 
@@ -24,7 +27,7 @@ class VirtualEnv:
 def create_env(venv: VirtualEnv):
     name = venv.name
 
-    print(f"Creating env {name}...")
+    rich.print(f"[bold green]Creating env {name}...[/bold green]")
 
     if venv.command:
         os.system(venv.command)
@@ -40,12 +43,12 @@ def create_env(venv: VirtualEnv):
 def install_deps(venv: VirtualEnv):
     name = venv.name
 
-    print("Installing deps")
+    rich.print(f"[bold green]...Installing deps for {name}[/bold green]")
 
     if venv.deps:
         local[f"envs/{name}/bin/pip"]["install", venv.deps]()
 
-    if not "numba" in venv.name:
+    if "numba" not in venv.name:
         local[f"envs/{name}/bin/python"]["-m", "pip", "install", "jinja2"]()
 
     # if venv.runner != "Cython" and venv.python not in {"python3.9", "python3.10"}:
@@ -55,14 +58,37 @@ def install_deps(venv: VirtualEnv):
     #     local[f"envs/{name}/bin/python"]["-m", "pip", "install", "jinja2"]()
 
 
+def destroy_env(venv: VirtualEnv):
+    name = venv.name
+    rich.print("[bold red]Something went wrong, destroying env[/bold red]")
+    rm_rf(f"envs/{name}")
+
+
+def rm_rf(path: str):
+    if Path(path).exists():
+        shutil.rmtree(path)
+
+
+def make_env(venv):
+    try:
+        create_env(venv)
+        install_deps(venv)
+        print()
+    except:
+        destroy_env(venv)
+
+
 def main(env=""):
     for env_dict in config.ENVS:
         venv = VirtualEnv(**env_dict)
         if env and env != venv.name:
             continue
 
-        create_env(venv)
-        install_deps(venv)
+        if Path(f"envs/{venv.name}").exists():
+            rich.print(f"[cyan]Env {venv.name} already exists, skipping[/cyan]")
+            continue
+
+        make_env(venv)
 
 
 if __name__ == "__main__":
